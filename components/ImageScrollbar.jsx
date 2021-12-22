89b09c1114c2562f185313ae6512afd039391fcc
codingshogun <kaushiksumralol538@gmail.com>
2021-12-23 02:21:44 +0530

    updated

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -43,8 +43,15 @@ export default function ImageSrollbar({ data }) {
       style={{ overflow: "hidden" }}
     >
       {data.map((item) => (
-        <Box width="910px" itemId={item.id} overflow="hidden" p="1">
+        <Box
+          width="910px"
+          itemId={item.id}
+          key={item.id}
+          overflow="hidden"
+          p="1"
+        >
           <Image
+            alt="homeImage"
             placeholder="blur"
             blurDataURL={item.url}
             src={item.url}
